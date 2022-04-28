30676aa58eda0fb61dd447477dcf374688a12472
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-28 02:02:02 -0500

    Se crea archivi index para pruebas manuales

---
+++ index.js
@@ -0,0 +1,9 @@
+const Reader = require ("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService")
+
+const explorers = Reader.readJonsonFile("explorers.json")
+console.log(explorers)
+
+console.log(ExplorerService.filterByMission(explorers, "node"))
+//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
+//console.log(ExplorerService.getExplorersUsernameByMission(explorers, "node"))
