ca40e65388da29a131d82e0d5028d260ca29173f
BernardoEspinoza02 <berespinozan@outlook.com>
2022-05-01 13:51:34 -0500

    Pruebas realizadas manuales con archivo index.js, y modificacion de la clase en explorerService.js

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const ExplorerService = require("./lib/services/ExplorerService")
 const explorers = Reader.readJonsonFile("explorers.json")
 //console.log(explorers)
 
-//console.log(ExplorerService.filterByMission(explorers, "node"))
-//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
-console.log(ExplorerService.getExplorersUsernameByMission(explorers, "githubUsername"))
+console.log(ExplorerService.filterByMission(explorers, "node"))
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
