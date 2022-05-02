28d6a3f6f0db4f82107ea8796993e29e40a7d0c4
BernardoEspinoza02 <berespinozan@outlook.com>
2022-05-02 00:49:17 -0500

    Pruebas manuales de impresion con fizzbuzzservices y explorerservices

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Reader = require ("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService")
+const FizzbuzzService = require("./lib/services/FizzbuzzService")
 
 const explorers = Reader.readJonsonFile("explorers.json")
 //console.log(explorers)
 
-console.log(ExplorerService.filterByMission(explorers, "node"))
-console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
+//console.log(ExplorerService.filterByMission(explorers, "node"))
+//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
+//console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+console.log(FizzbuzzService.applyValidationInExplorer(explorers, number))
