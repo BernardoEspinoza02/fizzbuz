ffe1b6d81b7e7da044b1639626769e1be5c032d0
BernardoEspinoza02 <berespinozan@outlook.com>
2022-05-02 00:50:35 -0500

    Modificacion del codigo en las estructuras para pruebas

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,21 @@
+const ExplorerService = require("../../lib/services/ExplorerService");
+
+describe("Tests para ExplorerService", () => {
+    test ("Requerimiento 1: Calcular todos los explorers en una mision", () => {
+        const explorers = [{ mission : "node" }];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    });
+
+    test ("Mostrar los explorers que pertenencan a la mission node", () => {
+        const explorers = [{mission : "node"}];
+        const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(explorersInNode.mission).toBe();
+    });
+
+    test ("Muestra el nombre del Usuario", () => {
+        const explorers = [{mission : "node"}];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorersInNode.mission)
+    })
+})
